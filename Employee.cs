59d13129f7afcd380acec9c6f4a8ec41365d8499
Alexander <coolweak2@gmail.com>
2019-02-16 16:10:34 +0300

    йцук

---
+++ Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private int _age;
+        private string _surname;
+
+        public Employee() { }
+        public Employee(string surname, int age)
+        {
+            this._surname = surname;
+            this._age = age;
+        }
+
+        private string GetAge()
+        {
+            return _age.ToString();
+        }
+
+        public void SetSurname(string surname)
+        {
+            this._surname = surname;
+        }
+
+        public string EmployeeInfo()
+        {
+            return $"Surname: {this._surname}, Age: {this._age}";
+        }
+    }
+}
